4152ad5cfaddede112576d303e025c372926ce30
Teddy Walter <teddywalter2016@gmail.com>
2021-02-25 02:44:25 +0000

    Config Route approprie au Gerant pour mesure de securite

--- modules/Gerant/Config/Routes.php
+++ modules/Gerant/Config/Routes.php
@@ -2,3 +2,8 @@
 
 $routes->setDefaultNamespace('Modules\Gerant\Controllers');
 $routes->get('gerant-dashboard','Dashboard::index',['filter' => 'isLoggedIn']);
+$routes->get('gerant-achat-list','Dashboard::getAchatList',['filter' => 'isLoggedIn']);
+$routes->get('gerant-stock','Dashboard::getStock',['filter' => 'isLoggedIn']);
+$routes->get('gerant-histo-appro','Dashboard::getHistoriqueAppro',['filter' => 'isLoggedIn']);
+$routes->get('gerant-histo-appro-inter-depot','Dashboard::getHistoriqueApproInterDepot',['filter' => 'isLoggedIn']);
+$routes->get('gerant-config-system','Dashboard::getConfigSystem',['filter' => 'isLoggedIn']);
--- modules/Gerant/Controllers/Dashboard.php
+++ modules/Gerant/Controllers/Dashboard.php
@@ -4,8 +4,39 @@ namespace Modules\Gerant\Controllers;
 class Dashboard extends BaseController {
   public function index(){
     $data = [
-      'titlePage' => 'Tableau de Bord: Caissier'
+      'titlePage' => 'Tableau de Bord: Gérant'
     ];
     echo view($this->linkMod.'\dashboard-view', $data);
+  }
+
+  public function getAchatList(){
+    $data = [
+      'titlePage' => 'Achat : Gérant'
+    ];
+    echo view('Modules\Admin\Views\achat\achat-list-view', $data);
+  }
+  public function getStock(){
+    $data = [
+      'titlePage' => 'Dépôts : Gérant'
+    ];
+    echo view('Modules\Admin\Views\stock\depots-stock-view', $data);
+  }
+  public function getHistoriqueAppro(){
+    $data = [
+      'titlePage' => 'Approvisionnement : Gérant'
+    ];
+    echo view('Modules\Admin\Views\appro\appro-historique-view', $data);
+  }
+  public function getHistoriqueApproInterDepot(){
+    $data = [
+      'titlePage' => 'Approvisionnement Inter-Dépôt : Gérant'
+    ];
+    echo view('Modules\Admin\Views\appro\appro-inter-depot-historique-view', $data);
+  }
+  public function getConfigSystem(){
+    $data = [
+      'titlePage' => 'Action Avancée : Gérant'
+    ];
+    echo view('Modules\Admin\Views\config\config-avance-view', $data);
   }
 }
