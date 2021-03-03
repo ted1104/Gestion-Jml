b05ccafdce7dc0055c1052ab6131a01e425a50d2
Teddy Walter <teddywalter2016@gmail.com>
2021-03-03 01:17:19 +0000

    Oublie fix filtre isManager modules admin

--- modules/Admin/Config/Routes.php
+++ modules/Admin/Config/Routes.php
@@ -2,27 +2,27 @@
 
 $routes->setDefaultNamespace('Modules\Admin\Controllers');
 $routes->get('admin-dashboard','Dashboard::index',['filter' => 'isManager']);
-$routes->get('admin-add-article','Articles::index',['filter' => 'isLoggedIn']);
-$routes->get('admin-add-appro','Approv::index',['filter' => 'isLoggedIn']);
-$routes->get('admin-list-achat','Achat::index',['filter' => 'isLoggedIn']);
-$routes->get('admin-list-negotiation-achat','Achat::negotiation_list',['filter' => 'isLoggedIn']);
-$routes->get('admin-list-article','Articles::list',['filter' => 'isLoggedIn']);
-$routes->get('admin-histo-appro','Approv::historique',['filter' => 'isLoggedIn']);
-$routes->get('admin-stock','Approv::stockDepots',['filter' => 'isLoggedIn']);
-$routes->get('admin-config-depot','Dashboard::config_depot',['filter' => 'isLoggedIn']);
-$routes->get('admin-config-etat-critique','Dashboard::config_critique',['filter' => 'isLoggedIn']);
-$routes->get('admin-caisse','Dashboard::getCaissierMontant',['filter' => 'isLoggedIn']);
-$routes->get('admin-encaissement-interne','Dashboard::getEncaissementInterne',['filter' => 'isLoggedIn']);
-$routes->get('admin-decaissement-externe','Dashboard::getDecaissementInterne',['filter' => 'isLoggedIn']);
-$routes->get('admin-add-users','Dashboard::createUsers',['filter' => 'isLoggedIn']);
-$routes->get('admin-list-users','Dashboard::getAllUsers',['filter' => 'isLoggedIn']);
-$routes->get('admin-histo-appro-inter-depot','Approv::historiqueInterDepot',['filter' => 'isLoggedIn']);
-$routes->get('admin-encaissement-externe','Dashboard::getEncaissementExterne',['filter' => 'isLoggedIn']);
-$routes->get('admin-stock-pv','Dashboard::getStockPv',['filter' => 'isLoggedIn']);
-$routes->get('admin-rapport','Dashboard::getRapport',['filter' => 'isLoggedIn']);
-$routes->get('admin-list-achat-partiel','Achat::getAchatPartiel',['filter' => 'isLoggedIn']);
-$routes->get('admin-config-system','Dashboard::getConfigSystem',['filter' => 'isLoggedIn']);
-$routes->get('admin-histo-transfert','Approv::getHistoTransfert',['filter' => 'isLoggedIn']);
+$routes->get('admin-add-article','Articles::index',['filter' => 'isManager']);
+$routes->get('admin-add-appro','Approv::index',['filter' => 'isManager']);
+$routes->get('admin-list-achat','Achat::index',['filter' => 'isManager']);
+$routes->get('admin-list-negotiation-achat','Achat::negotiation_list',['filter' => 'isManager']);
+$routes->get('admin-list-article','Articles::list',['filter' => 'isManager']);
+$routes->get('admin-histo-appro','Approv::historique',['filter' => 'isManager']);
+$routes->get('admin-stock','Approv::stockDepots',['filter' => 'isManager']);
+$routes->get('admin-config-depot','Dashboard::config_depot',['filter' => 'isManager']);
+$routes->get('admin-config-etat-critique','Dashboard::config_critique',['filter' => 'isManager']);
+$routes->get('admin-caisse','Dashboard::getCaissierMontant',['filter' => 'isManager']);
+$routes->get('admin-encaissement-interne','Dashboard::getEncaissementInterne',['filter' => 'isManager']);
+$routes->get('admin-decaissement-externe','Dashboard::getDecaissementInterne',['filter' => 'isManager']);
+$routes->get('admin-add-users','Dashboard::createUsers',['filter' => 'isManager']);
+$routes->get('admin-list-users','Dashboard::getAllUsers',['filter' => 'isManager']);
+$routes->get('admin-histo-appro-inter-depot','Approv::historiqueInterDepot',['filter' => 'isManager']);
+$routes->get('admin-encaissement-externe','Dashboard::getEncaissementExterne',['filter' => 'isManager']);
+$routes->get('admin-stock-pv','Dashboard::getStockPv',['filter' => 'isManager']);
+$routes->get('admin-rapport','Dashboard::getRapport',['filter' => 'isManager']);
+$routes->get('admin-list-achat-partiel','Achat::getAchatPartiel',['filter' => 'isManager']);
+$routes->get('admin-config-system','Dashboard::getConfigSystem',['filter' => 'isManager']);
+$routes->get('admin-histo-transfert','Approv::getHistoTransfert',['filter' => 'isManager']);
 
 
 
