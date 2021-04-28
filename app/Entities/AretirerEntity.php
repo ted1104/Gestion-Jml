48c2b6d413482d2c058235d63072cf138d64e3e4
Teddy Walter <teddywalter2016@gmail.com>
2021-04-28 17:28:38 +0100

    add retour user a qui a livre la qte

--- app/Entities/AretirerEntity.php
+++ app/Entities/AretirerEntity.php
@@ -4,6 +4,7 @@ use CodeIgniter\Entity;
 use Config\Services;
 use App\Models\ArticlesModel;
 use App\Models\StEtatCritiqueModel;
+use App\Models\UsersModel;
 
 class AretirerEntity extends Entity{
 
@@ -17,15 +18,18 @@ class AretirerEntity extends Entity{
     'deleted_at' => null,
   ];
 
-  // protected $datamap = [];
-  // protected static $articlesModel = null;
+  protected $datamap = [];
+  protected static $userModel = null;
   // protected static $etatcritique = null;
-  // public function __construct(array $data = null){
-  //   parent::__construct($data);
-  //   self::$articlesModel = new ArticlesModel();
-  //   self::$etatcritique = new StEtatCritiqueModel();
-  //
-  // }
+  public function __construct(array $data = null){
+    parent::__construct($data);
+    self::$userModel = new UsersModel();
+    // self::$etatcritique = new StEtatCritiqueModel();
+
+  }
+  public function getUsersId(){
+    return self::$userModel->select('id,nom,prenom')->Where('id',$this->attributes['users_id'])->find();
+  }
   //
   // protected function getArticlesId(){
   //   return self::$articlesModel->select('id,code_article,nom_article,description,logic_detail_data')->Where('id',$this->attributes['articles_id'])->find();
