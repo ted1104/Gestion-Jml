92fa7a9707ab285e25c3a14077742c373525021a
Teddy Walter <teddywalter2016@gmail.com>
2021-06-22 21:40:30 +0200

    model and entity pour cloture stock personnel

---
+++ app/Entities/CloturePersonnelStockEntity.php
@@ -0,0 +1,53 @@
+<?php namespace App\Entities;
+
+use CodeIgniter\Entity;
+use Config\Services;
+use App\Models\ArticlesModel;
+use App\Models\StEtatCritiqueModel;
+
+class CloturePersonnelStockEntity extends Entity{
+
+  protected $attributes = [
+    'id' => null,
+    'articles_id' => null,
+    'users_id' => null,
+    'qte_stock' => null,
+    'date_cloture'=>null,
+    'created_at' => null,
+    'updated_at' => null,
+    'deleted_at' => null,
+  ];
+
+  protected $datamap = [];
+  protected static $articlesModel = null;
+  protected static $etatcritique = null;
+  public function __construct(array $data = null){
+    parent::__construct($data);
+    self::$articlesModel = new ArticlesModel();
+    self::$etatcritique = new StEtatCritiqueModel();
+
+  }
+
+  public function getDateCloture(){
+    return $this->attributes['created_at'];
+  }
+
+  // protected function getArticlesId(){
+  //   return self::$articlesModel->select('id,code_article,nom_article,logic_detail_data')->Where('id',$this->attributes['articles_id'])->find();
+  // }
+  // protected function getLogicEtatCritique(){
+  //
+  //   $etat = self::$etatcritique->find();
+  //   $min = $etat[0]->montant_min;
+  //   $max = $etat[0]->montant_max;
+  //   $qte = $this->attributes['qte_stock_virtuel'];
+  //   return ($qte <= $min)?'1':($min < $qte && $qte < $max ?'2':'3');
+  // }
+
+
+
+
+
+
+
+}
---
+++ app/Models/CloturePersonnelStockModel.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+
+class CloturePersonnelStockModel extends Model{
+  protected $table = 'g_interne_cloture_personnel_stock';
+  protected $DBGroup = 'default';
+  protected $primaryKey = 'id';
+  protected $allowedFields = ['articles_id','users_id','qte_stock','date_cloture'];
+  protected $useTimestamps = true;
+  protected $validationRules = [
+    'articles_id' => 'required',
+    'depot_id' => 'required',
+    'qte_stock' => 'required',
+    'date_cloture' => 'required'
+  ];
+  protected $validationMessages = [
+    'articles_id'=>['required' => 'Le champ articles_id est obligatoire',],
+    'depot_id'=>['required' => 'Le champ depot_id est obligatoire'],
+    'qte_stock'=>['required' => 'Le champ qte_stock est obligatoire'],
+    'date_cloture' => ['required' => 'La date est obligatoire']
+  ];
+  protected $returnType ='App\Entities\CloturePersonnelStockEntity';
+}
